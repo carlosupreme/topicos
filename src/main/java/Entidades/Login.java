47442ca8bb2ae333f36a8abb0c9497d1fcde7703
Carlos Sosa <carlos.alberto.sosa.perera@gmail.com>
2023-02-24 20:54:40 -0600

    removing innecesary commits

--- src/main/java/Entidades/Login.java
+++ src/main/java/Entidades/Login.java
@@ -1,15 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Entidades;
 
 import java.util.HashMap;
 
-/**
- *
- * @author carlos
- */
+
 public class Login {
     private HashMap<String, String> data;
 
--- src/main/java/Topicos.java
+++ src/main/java/Topicos.java
@@ -1,13 +1,6 @@
 
 import Vistas.Login;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-/**
- *
- * @author carlos
- */
 public class Topicos {
 
     public static void main(String[] args) {
